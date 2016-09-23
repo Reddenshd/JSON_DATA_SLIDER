90f1da48ad763b852a910f89f45900621a100f95
Reddenshd <reddensrealm@gmail.com>
2016-09-23 15:57:39 -0400

    JS Update
    JS Update to read width of screen on 1st load and also on resize
    detection.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,28 @@
 // JavaScript Document
 
-var SliderImage;
-var SliderImageSM;
-var SliderHref;
-var slider;
+var SliderImage, SliderImageSM, SliderHref;
+var slider, TotalObject, index, SliderContainerATT;
+var total, WidthD;
 var counterSlide = 1;
-var TotalObject;
-var index;
-var SliderContainerATT;
-var total;
-var WidthD;
 
 function sliderActive() {
 	for(i = 0; i < SliderAccess.length; i++){
-			index = i;		
-			TotalObject = SliderAccess.length;						
-			SliderImage = SliderAccess[0].image;	
-			SliderImageSM = SliderAccess[0].SliderImageSM;	
-			SliderHref = SliderAccess[0].imageHref;	
-			
+		index = i;		
+		TotalObject = SliderAccess.length;						
+		SliderImage = SliderAccess[0].image;	
+		SliderImageSM = SliderAccess[0].SliderImageSM;	
+		SliderHref = SliderAccess[0].imageHref;	
 		if(i > 1) {$(".leftArrow, .rightArrow").css("display", "block");}
-		else{$(".leftArrow, .rightArrow").css("display", "none");}								 
+		else{$(".leftArrow, .rightArrow").css("display", "none");}							 
 	}
-	
 	document.getElementById('SliderContainer').setAttribute('total', TotalObject);
-	
-	        slider = document.createElement("img");
-			slider.id = "Slider_Main";
-			slider.src = SliderImage;
-			slider.setAttribute('href', SliderHref);			
-			$("#SliderContainer").append(slider);
-	
+	slider = document.createElement("img");
+	slider.id = "Slider_Main";
+	slider.src = SliderImage;
+	slider.setAttribute('href', SliderHref);			
+	$("#SliderContainer").append(slider);
 	$(".leftArrow").click(function(){leftArrow();});
 	$(".rightArrow").click(function(){rightArrow();});
-	
 	$(".leftArrow").mouseenter(function(){$(".leftArrow").animate({opacity: "1.0", height: "60px", width: "90px", top: "150px",  left: "9%", paddingTop: "30px"});});
 	$(".leftArrow").mouseleave(function(){$(".leftArrow").animate({opacity: "0.5", height: "40px", width: "50px", top: "175px", left: "10%", paddingTop: "10px"});});
 	$(".rightArrow").mouseenter(function(){$(".rightArrow").animate({opacity: "1.0", height: "60px", width: "90px", top: "150px", right: "9%", paddingTop: "30px"});});
@@ -48,9 +37,10 @@ function sliderActive() {
 		counterSlide++; 
 		if (counterSlide > TotalObject){counterSlide = 1;} 
 			setSlide() ;
-		}
+		}	
+		setSlide();
 }
-function action(){SliderContainerATT = document.getElementById("SliderContainer").getAttribute('slide');
+  function action(){SliderContainerATT = document.getElementById("SliderContainer").getAttribute('slide');
 }
 
 function setSlide() {
