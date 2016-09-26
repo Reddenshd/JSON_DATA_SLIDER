9bf0995bc1db2c3c20f845b1d3196e33a7aa9e3b
Reddenshd <reddensrealm@gmail.com>
2016-09-26 10:55:01 -0400

    Href Linkage click out
    Added Href linkage change upon data

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 // JavaScript Document
 
-var SliderImage, SliderImageSM, SliderHref;
-var slider, TotalObject, index, SliderContainerATT;
-var total, WidthD, SliderHREFset;
-var folderLoc = 'images/', smlImageIndex = 'sm-';
-var counterSlide = 1;
+var SliderImage, SliderImageSM, SliderHref, slider, TotalObject, index, SliderContainerATT;
+var total, WidthD, SliderHREFset, folderLoc = 'images/', smlImageIndex = 'sm-', counterSlide = 1;
 
 function sliderActive() {
 	for(i = 0; i < SliderAccess.length; i++){
@@ -18,10 +15,14 @@ function sliderActive() {
 	}
 	document.getElementById('SliderContainer').setAttribute('total', TotalObject);
 	slider = document.createElement("img");
+	SliderHREF = document.createElement("a");
 	slider.id = "Slider_Main";
-	slider.src = SliderImage;
-	slider.setAttribute('href', SliderHref);			
+	SliderHREF.id = "SliderHREF";
+	//SliderHREF.src = SliderImage;
+	SliderHREF.setAttribute('href', SliderHref);			
 	$("#SliderContainer").append(slider);
+	$("#SliderContainer").append(SliderHREF);
+	$("#SliderHREF").attr("target", "_blank");
 	$(".leftArrow").click(function(){leftArrow();});
 	$(".rightArrow").click(function(){rightArrow();});
 	
@@ -33,22 +34,27 @@ function sliderActive() {
 	  $(".rightArrow").mouseenter(function(){$(".rightArrow").animate({opacity: "1.0", height: "60px", width: "90px", top: "150px", right: "9%", paddingTop: "30px"});});
 	  $(".rightArrow").mouseleave(function(){$(".rightArrow").animate({opacity: "0.5", height: "40px", width: "50px", top: "175px", right: "10%", paddingTop: "10px"});});
     };
-	function leftArrow() {counterSlide--; if (counterSlide < 1){counterSlide = TotalObject;} setSlide();}
-	function rightArrow() {counterSlide++; if (counterSlide > TotalObject){counterSlide = 1;} setSlide();}	
+	function leftArrow() {counterSlide--; if (counterSlide < 1){counterSlide = TotalObject;} 	SliderHREF.setAttribute('href', SliderAccess[counterSlide - 1].imageHref);	 setSlide();}
+	function rightArrow() {counterSlide++; if (counterSlide > TotalObject){counterSlide = 1;} SliderHREF.setAttribute('href', SliderAccess[counterSlide - 1].imageHref); setSlide();}	
 	setSlide();
 }
-  function action(){SliderContainerATT = document.getElementById("SliderContainer").getAttribute('slide');}
- // function urlSet(){SliderHREFset = document.getElementById("Slider_Main").setAttribute("href", );}
+function action(){SliderContainerATT = document.getElementById("SliderContainer").getAttribute('slide');}
 
 function setSlide() {
   WidthD = document.body.clientWidth;
   if(WidthD <= 768){
 	  document.getElementById("Slider_Main").src = folderLoc + smlImageIndex + counterSlide +'.jpg'; 
-  	  // urlSet();
+	  document.getElementById('SliderHREF').style.width = '300px';
+	  document.getElementById('SliderHREF').style.height = '250px';
+	  document.getElementById('SliderHREF').style.top = '-177px';
+	  SliderContainerATT = document.getElementById("SliderContainer").setAttribute("slide", counterSlide);
   }
-  else{ document.getElementById("Slider_Main").src = folderLoc + counterSlide +'.jpg'; }
-  SliderContainerATT = document.getElementById("SliderContainer").setAttribute("slide", counterSlide);
-  // urlSet();
+  else{ 
+   document.getElementById("Slider_Main").src = folderLoc + counterSlide +'.jpg'; 
+   document.getElementById('SliderHREF').style.width = '600px';
+   document.getElementById('SliderHREF').style.height = '300px';
+   document.getElementById('SliderHREF').style.top = '-226px';
+   SliderContainerATT = document.getElementById("SliderContainer").setAttribute("slide", counterSlide);
+  }
 }
-
 window.onresize = function(event) {setSlide();};
